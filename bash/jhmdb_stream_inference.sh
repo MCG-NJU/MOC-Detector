02b655b62755c49d58edf1bb94006bd524118f93
zixuwang <zixuwang1997@gmail.com>
2020-07-09 23:31:39 +0800

    modify bash

--- bash/jhmdb_stream_inference.sh
+++ bash/jhmdb_stream_inference.sh
@@ -1,44 +1,14 @@
 cd src
-# python3 det.py --task stream --K 7 --gpus 0 --batch_size 1 --master_batch 1 --num_workers 0 --rgb_model ../experiment/result_model/$PATH_TO_RGB_MODEL --flow_model ../experiment/result_model/$PATH_TO_FLOW_MODEL --inference_dir $INFERENCE_DIR --flip_test --ninput 5 --dataset hmdb --split 1 --hm_fusion_rgb 0.4
-#
-# python3 ACT.py --task frameAP --K 7 --th 0.5 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
-#
-# python3 ACT.py --task BuildTubes --K 7 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
-#
-# python3 ACT.py --task videoAP --K 7 --th 0.2 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
-# python3 ACT.py --task videoAP --K 7 --th 0.5 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
-# python3 ACT.py --task videoAP --K 7 --th 0.75 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
-# python3 ACT.py --task videoAP_all --K 7 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
-#
+python3 det.py --task stream --K 7 --gpus 0 --batch_size 1 --master_batch 1 --num_workers 0 --rgb_model ../experiment/result_model/$PATH_TO_RGB_MODEL --flow_model ../experiment/result_model/$PATH_TO_FLOW_MODEL --inference_dir $INFERENCE_DIR --flip_test --ninput 5 --dataset hmdb --split 1 --hm_fusion_rgb 0.4
 
-python3 det.py --task stream --K 7 --gpus 0 --batch_size 1 --master_batch 1 --num_workers 0 --rgb_model ../experiment/result_model/hmdb_s1_dla34_K7_rgb_coco.pth --flow_model ../experiment/result_model/hmdb_s1_dla34_K7_flow_coco.pth --inference_dir /data0/liyixuan/speed_test/test --flip_test --ninput 5 --dataset hmdb --split 1 --hm_fusion_rgb 0.4
+python3 ACT.py --task frameAP --K 7 --th 0.5 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
 
+python3 ACT.py --task BuildTubes --K 7 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
 
-python3 ACT.py --task frameAP --K 7 --th 0.5 --inference_dir /data0/liyixuan/speed_test/test --dataset hmdb --split 1
-
-python3 ACT.py --task BuildTubes --K 7 --inference_dir /data0/liyixuan/speed_test/test --dataset hmdb --split 1
-
-python3 ACT.py --task videoAP --K 7 --th 0.2 --inference_dir /data0/liyixuan/speed_test/test --dataset hmdb --split 1
-python3 ACT.py --task videoAP --K 7 --th 0.5 --inference_dir /data0/liyixuan/speed_test/test --dataset hmdb --split 1
-python3 ACT.py --task videoAP --K 7 --th 0.75 --inference_dir /data0/liyixuan/speed_test/test --dataset hmdb --split 1
-python3 ACT.py --task videoAP_all --K 7 --inference_dir /data0/liyixuan/speed_test/test --dataset hmdb --split 1
-
-
-# python3 det.py --task stream --K 7 --gpus 0 --batch_size 1 --master_batch 1 --num_workers 0 --rgb_model ../experiment/result_model/hmdb_s1_dla34_K7_rgb_coco.pth --flow_model ../experiment/result_model/hmdb_s1_dla34_K7_flow_coco.pth --inference_dir /data0/liyixuan/speed_test/test20 --ninput 5 --flip_test --dataset hmdb --split 1 --hm_fusion_rgb 0.4
-#
-#
-# python3 ACT.py --task frameAP --K 7 --th 0.5 --inference_dir /data0/liyixuan/speed_test/test20 --dataset hmdb --split 1
-#
-# python3 ACT.py --task BuildTubes --K 7 --inference_dir /data0/liyixuan/speed_test/test20 --dataset hmdb --split 1
-#
-# python3 ACT.py --task videoAP --K 7 --th 0.2 --inference_dir /data0/liyixuan/speed_test/test20 --dataset hmdb --split 1
-# python3 ACT.py --task videoAP --K 7 --th 0.5 --inference_dir /data0/liyixuan/speed_test/test20 --dataset hmdb --split 1
-# python3 ACT.py --task videoAP --K 7 --th 0.75 --inference_dir /data0/liyixuan/speed_test/test20 --dataset hmdb --split 1
-# python3 ACT.py --task videoAP_all --K 7 --inference_dir /data0/liyixuan/speed_test/test20 --dataset hmdb --split 1
-#
-#
+python3 ACT.py --task videoAP --K 7 --th 0.2 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
+python3 ACT.py --task videoAP --K 7 --th 0.5 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
+python3 ACT.py --task videoAP --K 7 --th 0.75 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
+python3 ACT.py --task videoAP_all --K 7 --inference_dir $INFERENCE_DIR --dataset hmdb --split 1
 
 
 cd ..
-
-# python3 det.py --task speed_test --K 7 --gpus 0 --batch_size 1 --master_batch 1 --rgb_model ../experiment/result_model/hmdb_s1_dla34_K7_rgb_coco.pth --flow_model ../experiment/result_model/hmdb_s1_dla34_K7_flow_coco.pth --inference_dir /data0/liyixuan/speed_test/test2 --ninput 5 --dataset hmdb --split 1  --num_workers 0 --flip_test
