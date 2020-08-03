bb3824be60d0e369e9c7b3667e95b43a571bba33
zixuwang <zixuwang1997@gmail.com>
2020-08-04 01:30:48 +0800

    delete speed_inference.py and speed_moc_det.py

--- src/det.py
+++ src/det.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from opts import opts
 from inference.normal_inference import normal_inference
 from inference.stream_inference import stream_inference
-from inference.speed_inference import speed_inference_backbone, speed_inference_det
+# from inference.speed_inference import speed_inference_backbone, speed_inference_det
 import torch
 import random
 import time
@@ -27,10 +27,10 @@ if __name__ == '__main__':
         normal_inference(opt)
     elif opt.task == 'stream':
         stream_inference(opt)
-    elif opt.task == 'speed_backbone':
-        speed_inference_backbone(opt)
-    elif opt.task == 'speed_branch':
-        speed_inference_det(opt)
+    # elif opt.task == 'speed_backbone':
+    #     speed_inference_backbone(opt)
+    # elif opt.task == 'speed_branch':
+    #     speed_inference_det(opt)
     elif opt.task == 'speed_test':
         from inference.speed_test import speed_test_stream_inference
         speed_test_stream_inference(opt)
