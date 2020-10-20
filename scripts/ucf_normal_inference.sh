50096829823f0c107a539c6dfc907e7b1c5e3ce2
zixuwang <zixuwang1997@gmail.com>
2020-10-20 17:23:05 +0800

    revise N=10 w/o flip

--- scripts/ucf_normal_inference.sh
+++ scripts/ucf_normal_inference.sh
@@ -1,5 +1,5 @@
 cd src
-# [News] ------  We recommend using '--N 10' and remove '--flip_test'
+# [News] ------  Using '--N 10' and remove '--flip_test will increase the inference speed but get a lower performance.'
 
 python3 det.py --task normal --K 7 --gpus 0,1,2,3,4,5,6,7 --batch_size 94 --master_batch 10 --num_workers 8 --rgb_model ../experiment/result_model/$PATH_TO_RGB_MODEL --flow_model ../experiment/result_model/$PATH_TO_FLOW_MODEL --inference_dir $INFERENCE_DIR --flip_test --ninput 5
 python3 det.py --task normal --K 7 --gpus 0 --batch_size 1 --master_batch 1 --num_workers 0 --rgb_model ../experiment/result_model/$PATH_TO_RGB_MODEL --flow_model ../experiment/result_model/$PATH_TO_FLOW_MODEL --inference_dir $INFERENCE_DIR --flip_test --ninput 5
